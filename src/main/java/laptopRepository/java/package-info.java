85184adfed404eeb9a6db16c556b7d680e9488f0
ShikhaSharma20 <shikha.sharma80634@gmail.com>
2022-10-30 12:44:27 +0530

    first commit

---
+++ src/main/java/laptopRepository/java/package-info.java
@@ -0,0 +1,11 @@
+package laptopRepository.java;package com.devlaptop.repository;
+
+import com.devlaptop.model.laptop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface laptopRepository extends JpaRepository<laptop,Integer> {
+
+}
